da93bad2c98d496bbbec74b7752f014bf84e44ec
XuanPhuc <xuanphuca1k66@gmail.com>
2021-11-17 12:45:22 +0700

    fix register

--- Trung/assets/javascript/form.js
+++ Trung/assets/javascript/form.js
@@ -1,15 +1,3 @@
-var inputName = document.getElementById('input__name')
-var inputPassword = document.getElementById('input--password')
-
-function dangnhap(a) {
-    if (inputName.value == '' || inputPassword.value == '') {
-        alert('Email hoặc mật khẩu không hợp lệ')
-    } else {
-
-    }
-}
-
-
 var inputPasswordDK = document.getElementById('input__password--dk')
 var Confirm = document.getElementById('confirm')
 
@@ -18,15 +6,32 @@ var inputName = document.getElementById('input__name')
 var inputSdt = document.getElementById('input__sdt')
 
 
-function dangki(a) {
-    console.log(Confirm)
-    if (inputPasswordDK.value != Confirm.value) {
-        alert('Mật khẩu và xác nhận mật khẩu không trùng nhau!')
-    }
-    if (inputPasswordDK.value != Confirm.value) {
-        alert('Mật khẩu và xác nhận mật khẩu không trùng nhau!')
-    }
-    if (inputEmail.value == '' || inputPassword.value == '' || inputName.value == '' || inputSdt.value == '' || Confirm.value == '') {
+
+function dangKi(a) {
+
+    console.log(inputName.value)
+    if (inputEmail.value == '' || inputPasswordDK.value == '' || inputName.value == '' || inputSdt.value == '' || Confirm.value == '') {
         alert('Chưa nhập đầy đủ thông tin!')
+
+    } else {
+        if (inputPasswordDK.value != Confirm.value) {
+            console.log(1)
+            alert('Mật khẩu và xác nhận mật khẩu không trùng nhau!')
+        } else {
+            alert('đăng kí thành công')
+        }
+    }
+}
+
+
+// dang nhap  
+var inputName = document.getElementById('input__name')
+var inputPassword = document.getElementById('input--password')
+
+function dangnhap(a) {
+    if (inputName.value == '' || inputPassword.value == '') {
+        alert('Email hoặc mật khẩu không hợp lệ')
+    } else {
+        alert('đăng nhập thành công')
     }
 }
